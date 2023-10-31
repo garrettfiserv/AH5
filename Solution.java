484c1126b9b6fdce9235002a95ccf22572836c88
Boscoe <garrett.boscoe@fiserv.com>
2023-10-31 14:55:20 -0400

    solved AH5

--- Solution.java
+++ Solution.java
@@ -1,12 +1,34 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public static char mostFrequentChar(String s) {
-      
-      return;
+    public static Object mostFrequentChar(String s) {
+      HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+      int temp =0;
+      int max = 0;
+      Object ans = null;
+      for (char a: s.toCharArray()){
+        if(!map.containsKey(a)){
+          map.put(a, 1);
+        }else{
+          map.put(a, ((int)map.get(a)+1));
+        }
+      }
+
+      Object[] charArr = map.keySet().toArray();
+      for (int i = 0; i< charArr.length;i++){
+        if((int)map.get(charArr[i]) >= max){
+          ans = charArr[i];
+          max = (int)map.get(charArr[i]);
+        } 
+      }
+      return ans;
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("david")); // -> 'e'
     }
 
   }
